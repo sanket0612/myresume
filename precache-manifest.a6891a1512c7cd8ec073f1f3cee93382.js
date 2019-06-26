eb9d3a448f7616e349aaee2e8402eccbdef7af50
sankubunny <sanket.barapatre.06@gmail.com>
2019-06-26 17:28:02 +0530

    Updates

---
+++ precache-manifest.a6891a1512c7cd8ec073f1f3cee93382.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0afe921ae9a9b911020ebbc76f2473f",
+    "url": "/myresume/index.html"
+  },
+  {
+    "revision": "a14bf3d29884b1c05719",
+    "url": "/myresume/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "9f2186fb6044641b6fd5",
+    "url": "/myresume/static/js/2.5ae3991e.chunk.js"
+  },
+  {
+    "revision": "a14bf3d29884b1c05719",
+    "url": "/myresume/static/js/main.8c4d140a.chunk.js"
+  },
+  {
+    "revision": "95e7c0f5a6680da6caf6",
+    "url": "/myresume/static/js/runtime~main.6f2b030a.js"
+  }
+]);
