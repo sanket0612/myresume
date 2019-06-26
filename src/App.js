0676153d05a833093d6e7159595cc5f28dd523d5
sankubunny <sanket.barapatre.06@gmail.com>
2019-06-26 17:50:20 +0530

    commit 4

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'/resumeData.json',
+      url:'/myresume/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
